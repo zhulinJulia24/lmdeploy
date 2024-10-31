94052a8c7e2274b9bc02357111c8d334aff302fc
zhulin1 <zhulin1@pjlab.org.cn>
2024-10-31 10:05:46 +0800

    update

--- autotest/utils/restful_return_check.py
+++ autotest/utils/restful_return_check.py
@@ -82,9 +82,9 @@ def assert_chat_completions_stream_return(output,
         if not is_last:
             assert message.get('finish_reason') is None
             if check_logprobs:
-                assert (len(message.get('logprobs').get('content')) == 1)
-                assert_logprobs(
-                    message.get('logprobs').get('content')[0], logprobs_num)
+                assert (len(message.get('logprobs').get('content')) >= 1)
+                for content in message.get('logprobs').get('content'):
+                    assert_logprobs(content, logprobs_num)
 
         if is_last is True:
             assert len(message.get('delta').get('content')) == 0
