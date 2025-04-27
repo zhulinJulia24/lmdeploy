ef0e687370903ddeb3a4c6e784418306dd9f6de6
zhulinJulia24 <145004780+zhulinJulia24@users.noreply.github.com>
2025-04-27 21:01:21 +0800

    Update run_restful_chat.py

--- autotest/utils/run_restful_chat.py
+++ autotest/utils/run_restful_chat.py
@@ -50,7 +50,7 @@ def start_restful_api(config, param, model, model_path, backend_type, worker_id)
         port = DEFAULT_PORT + worker_num
 
     cmd = get_command_with_extra('lmdeploy serve api_server ' + model_path + ' --session-len 8096 --server-port ' +
-                                 str(port),
+                                 str(port) + ' --log-level INFO',
                                  config,
                                  model,
                                  need_tp=True,
